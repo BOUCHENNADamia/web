968f5a999ecbf39517d38690e97148b54757b016
Gabin Caiveau <gabin.caiveau@gmail.com>
2026-07-21 11:27:07 +0800

    Final

--- frontend/src/components/Marquee.jsx
+++ frontend/src/components/Marquee.jsx
@@ -1,4 +1,4 @@
-const TRACK_TEXT = 'Kuala Lumpur ● Singapore ● Bangkok ● Perhentian Islands ● Melaka ● Putrajaya ● Penang ● Cameron Highlands ● ';
+const TRACK_TEXT = 'Kuala Lumpur ● Singapore ● Bangkok ● Perhentian Islands ● Melaka ● Putrajaya ● Penang ● Cameron Highlands ● Hong Kong ● Shenzhen ● ';
 
 export default function Marquee() {
   return (
